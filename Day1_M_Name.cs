bee466a0e7fc66ff25e6cedf8508b5e027d806c8
Laxmi-Mapari7 <116425049+Laxmi-Mapari7@users.noreply.github.com>
2023-11-22 11:16:47 +0530

    Update Day1_M_Name.cs

--- Day1_M_Name.cs
+++ Day1_M_Name.cs
@@ -1,1 +1,41 @@
+review1.cs
+int d; // elapsed time in days
+int elapsedTimeInDays;
 
+review2.cs
+var dataFromDb = db.GetFromService().ToList(); // Get List of employees 
+var listOfEmployees=db.GetFromService().ToList();
+
+review3.cs
+int iCounter;
+string strFullName;
+DateTime dModifiedDate;
+int counter;
+string fullName;
+DateTime modifiedDate;
+
+review4.cs
+public bool isShopOpen(string pDay, int pAmount)
+{
+    // some logic
+}
+
+review5.cs
+const int daysInWeek = 7;
+const int daysInMonth = 30;
+var songs = new List<string> { 'Back In Black', 'Stairway to Heaven', 'Hey Jude' };
+var artists = new List<string> { 'ACDC', 'Led Zeppelin', 'The Beatles' };
+class Animal {}
+class Alpaca {}
+
+review6.cs
+public Datetime sWorkDate { get; set; } // get set Start Working Date
+public Datetime startWorkDate { get; set; }
+public Datetime modTime { get; set; } // get set Modification Time
+public Datetime modificationTime { get; set; } 
+
+review7.cs
+public double calculateSalary(int workingdays, int workinghours)
+{
+    // some logic
+}
